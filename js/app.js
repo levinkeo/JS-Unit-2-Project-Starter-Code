9cef285c35af8a7b5d11fb054b3c9782d832da28
Kevin Leonard <kleonard@bosmm2qk2tpfvh8.local>
2016-05-17 21:31:22 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-/*
-  Please add all Javascript code to this file.
-*/
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': ""
+});
+var response;
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(data) {
+  console.log(data);
+   for(var i =0; i<data.results.length; i++){
+   	var articleHeadline = data.results[i].title;
+  	var articleByline = data.results[i].byline;
+   	var articleURL = data.results[i].url;
+   	var articleAllMedia = data.results[i].multimedia;
+   	function findGoldilocksImg(){
+   		for (var i=0; i < articleAllMedia.length; i++) {
+   			if(articleAllMedia[i].format == "Normal") {
+   				return articleAllMedia[i].url;
+   			}
+   		}
+   	};
+   	$('.results').append('<li><img src="'+ findGoldilocksImg(articleAllMedia) + '"><a href="'+ articleURL + '">' + articleHeadline +'</a></li>');
+   } 
+  response = data
+}).fail(function(err) {
+  throw err;
+})
